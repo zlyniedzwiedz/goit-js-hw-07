07d2e7c402c9e144fd33398cd58960420ea8198a
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-05 19:33:31 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+let instance = basicLightbox.create("")
+const galleryImage = document.querySelector("gallery_image")
+const imageHandler = (image) => {
+ image.preventDefault()
+ instance = basicLightbox.create(`<img class="gallery__image"
+ src="${image.target.getAttribute("data-source")}"
+alt="${image.target.alt}"
+/>`)
+instance.show()
+}
+
+const galleryMarkup = galleryItems
+  .map((image) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+
+  galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+  galleryList.addEventListener("click", imageHandler)
+
+  galleryList.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" ) {
+        instance.close()
+    }
+
+  })
+
+
+  //
+
+  
