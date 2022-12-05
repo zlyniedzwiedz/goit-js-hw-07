1e540c3dd559ee1c897da8e164e52f3f266921d1
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-05 19:48:37 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+  </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData: "alt", captionDelay: 250 /* options */ });
